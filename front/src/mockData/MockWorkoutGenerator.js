aaa76ef83ca60eefa5543ec5e305bf7cec83d074
Tommi Niittymies <tommi.niittymies@gmail.com>
2020-10-09 15:07:39 +0300

    Minor frontend work

--- front/src/mockData/MockWorkoutGenerator.js
+++ front/src/mockData/MockWorkoutGenerator.js
@@ -1,6 +1,20 @@
+import moveSetService from '../services/MoveSetService'
+
 const generateMockWorkout = () => {
+    let movesets = []
+
     for (i = 0; i < 5; i++) {
-        console.log('')
+        moveset = {
+            label: 'penkki',
+            repetitions: null
+        }
+        
+    }
+
+    const workout = {
+        date: new Date(),
+        archived: false,
+
     }
 }
 
