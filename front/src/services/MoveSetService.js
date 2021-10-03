feef1891da3a583aabd9775b331abf0de7247664
Tommi Niittymies <tommi.niittymies@gmail.com>
2021-10-03 20:28:47 +0300

    Fix bug in saving a Workout

--- front/src/services/MoveSetService.js
+++ front/src/services/MoveSetService.js
@@ -5,15 +5,13 @@ const baseURL = 'http://localhost:8080/api/moveset'
 
 const putChanged = async (id, reps) => {
     const url = `${baseURL}/${id}`
-    const config = GenerateTokenizedHeader()
     const payload = {
         newRepetitions: reps
     }
-    console.log('Moveset Service, id & reps: ', id, reps)
-    console.log('Moveset Service, url, payload & config: ', url, payload, config)
+    const config = GenerateTokenizedHeader()
+
     const response = await axios.put(url, payload, config)
     return response.data
-
 }
 
 export default { putChanged }
