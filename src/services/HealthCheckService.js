8c8069b7e11a8f09962598c3de3616ba6c2d854c
TommiON <tommi.niittymies@gmail.com>
2022-05-21 20:44:14 +0300

    A bit more of CORS testing

--- src/services/HealthCheckService.js
+++ src/services/HealthCheckService.js
@@ -6,6 +6,8 @@ const URL = `https://${backendURL}/api/healthcheck`
 const healthCheck = async () => {
     console.log('kutsutaan terveystarkastusta, url: ', URL)
     const response = await axios.get(URL)
+    console.log('serviceltä saadaan: ', response.data)
+    return response.data
 }
 
 export default { healthCheck }
